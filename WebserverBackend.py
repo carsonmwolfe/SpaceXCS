9e7ad1311fb76ec56cd0969a41088311b3664894
Carson <46657662+Darkarea51@users.noreply.github.com>
2020-12-07 10:43:08 -0800

    Update WebserverBackend.py

--- WebserverBackend.py
+++ WebserverBackend.py
@@ -62,18 +62,16 @@ def launchdetails():
     Upcoming = requests.get("https://api.spacexdata.com/v3/launches/upcoming").text
     json_Upcoming=json.loads(Upcoming)
 
-    details_string = "Mission Name:" + str(json_Upcoming[0]["mission_name"] + "\n" + "Rocket Model:" + str(json_Upcoming[0]["rocket"]["rocket_name"]))
+    details_string = "Mission Name:" + str(json_Latest)[0]["mission_name"] + "\n" + "Rocket Model:" + str(json_Latest[0]["rocket"]["rocket_name"]))
     return details_string
-
-
-
-
-
-@app.route('/launchinformation')
-def launchinformation():
 
 @app.route('/upcominglaunches')
 def upcominglaunches():
 
+    Upcoming = requests.get("https://api.spacexdata.com/v3/launches/upcoming").text
+    json_Upcoming=json.loads(Upcoming)
+
+    upcoming_string = "Mission Name:" + str(json_Upcoming)[0]["mission_name"] + "\n" + "Rocket Model:" + str(json_Upcoming[0]["rocket"]["rocket_name"])
+    return upcoming_string
 
 app.run(threaded=True,port=int(os.environ.get('PORT', 5000)))
